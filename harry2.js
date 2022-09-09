c44e19c17b40856f9bcd62fa6c2a715f361097b0
Hariharan09 <48160715+Hariharan09@users.noreply.github.com>
2022-09-09 11:21:24 +0530

    Add files via upload

---
+++ harry2.js
@@ -0,0 +1,48 @@
+// functions
+
+function getSum(x, y) {
+  return x + y;
+}
+//   let result = getSum(10,20)
+//   console.log(result)
+//   let a=50
+//   let b=60
+//   let result1 = getSum(a,b)
+//   console.log(result1)
+//   let result2 = getSum("react","native")
+//   console.log(result2)
+
+//   function getLargestno(a,b,c,d){
+//       if(a>b && a>c && a>d){
+//         return a
+//       }
+//       else if ( b>c && b>d){
+//         return b
+//       }
+//       else if (c>d){
+//         return c
+//       }
+//       else {
+//         return d
+//       }
+
+//   }
+//     let result = getLargestno(40,10,30,20)
+//     let result1 = getLargestno(80,60,90,50)
+//     console.log(result,result1)
+function getResult(x, y) {
+  for (let i = 0; i < x.length; i++) {
+    let data1 = x[i].toLowerCase();
+    // if (data1.indexOf(y.toLowerCase()) != -1) {
+    //   array.push(x[i]);
+    // }
+  //   data1.includes(y.toLowerCase())&&array.push(x[i])
+  // }
+  // return array;
+// }
+
+// let nameList = ["Jenny", "Marx", "Yengels", "Lenin", "Stalin", "Che", "castro"];
+// let searchKey = "n";
+// let array = [];
+// let searchedList = getResult(nameList, searchKey);
+// console.log(searchedList);
